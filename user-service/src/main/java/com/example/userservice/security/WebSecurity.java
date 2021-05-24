204c12439a28f188ea36c1722d0161004d0d2ce3
gos1004 <nnv711@gmail.com>
2021-05-25 01:43:53 +0900

    actuator 추가 bootstrap.yml 적용

--- user-service/src/main/java/com/example/userservice/security/WebSecurity.java
+++ user-service/src/main/java/com/example/userservice/security/WebSecurity.java
@@ -30,8 +30,9 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable();
 		//http.authorizeRequests().antMatchers("/users/**").permitAll();
-		http.authorizeRequests().antMatchers("/**")
-			.hasIpAddress("192.168.219.101")
+		http.authorizeRequests().antMatchers("/actuator/**").permitAll();
+		http.authorizeRequests().antMatchers("/users")
+			.hasIpAddress(env.getProperty("gateway.ip"))
 			.and()
 			.addFilter(getAuthenticationFilter());
 		
