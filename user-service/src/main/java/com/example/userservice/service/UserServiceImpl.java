cb743e12296a3a52169ad5c98db861bf1ece2dc4
gos1004 <nnv711@gmail.com>
2021-07-02 02:33:04 +0900

    RestTemplate 적용

--- user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
+++ user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -6,11 +6,16 @@ import java.util.UUID;
 
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.example.userservice.dto.UserDto;
 import com.example.userservice.jpa.UserEntity;
@@ -22,10 +27,15 @@ public class UserServiceImpl implements UserService{
 
 	UserRepository userRepository;
 	BCryptPasswordEncoder passwordEncoder;
-	
-	public UserServiceImpl(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
+	Environment env;
+	RestTemplate restTemplate;
+
+	public UserServiceImpl(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder, Environment env,
+			RestTemplate restTemplate) {
 		this.userRepository = userRepository;
 		this.passwordEncoder = passwordEncoder;
+		this.env = env;
+		this.restTemplate = restTemplate;
 	}
 
 	@Override
@@ -66,8 +76,17 @@ public class UserServiceImpl implements UserService{
 		
 		UserDto userDto = new ModelMapper().map(userEntity, UserDto.class);
 		
-		List<ResponseOrder> orders = new ArrayList<>();
-		userDto.setOrders(orders);
+		//List<ResponseOrder> orders = new ArrayList<>();
+		
+		/* Using as rest template */
+		String orderUrl = String.format(env.getProperty("order_service.url"), userId);
+		ResponseEntity<List<ResponseOrder>> orderListResponse = 
+				restTemplate.exchange(orderUrl, HttpMethod.GET, null, 
+						new ParameterizedTypeReference<List<ResponseOrder>>(){});
+		
+		List<ResponseOrder> ordersList = orderListResponse.getBody();
+		userDto.setOrders(ordersList);
+		
 		
 		return userDto;
 	}
